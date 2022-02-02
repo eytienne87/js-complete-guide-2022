328f47ec7f23d3d2a6488f218bf1706ef0290d18
Etienne Tessier <etienne_tessier100@hotmail.ca>
2022-02-02 17:24:43 -0500

    used for and for of loop to loop over numbers array and print the numbers to the concole

--- control-assignment-problem/assignment.js
+++ control-assignment-problem/assignment.js
@@ -1,8 +1,18 @@
 
-for (let i = 0; i < 10; i++) {
-  let randomNumber = Math.round((Math.random()) * 100) / 100; // produces random number between 0 (including) and 1 (excluding)
-  console.log(randomNumber);
-  if (randomNumber > 0.7) {
-    alert(`${randomNumber} is bigger than 0.7!`)
-  }
+// for (let i = 0; i < 10; i++) {
+//   let randomNumber = Math.round((Math.random()) * 100) / 100; // produces random number between 0 (including) and 1 (excluding)
+//   console.log(randomNumber);
+//   if (randomNumber > 0.7) {
+//     alert(`${randomNumber} is bigger than 0.7!`)
+//   }
+// }
+
+let numbers = [1, 3, 5, 7, 9];
+
+for (const number of numbers) {
+  console.log(`I am ${number}, a member of the numbers array!`);
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(`I am ${numbers[i]}, a member of the numbers array!`);
 }
